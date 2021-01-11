7e79785be4d97883b608eae632293aa466c50c62
Timwi <github@timwi.de>
2021-01-11 21:40:14 +0100

    Timwi version

--- Program.cs
+++ Program.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
-namespace TestChallengeTwix
+[assembly: AssemblyTitle("BrainFJit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BrainFJit")]
+[assembly: AssemblyCopyright("Copyright © Timwi 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ef410d12-518e-439c-b557-65b4be09c0ba")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+namespace BrainFJit
 {
     public class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            var gameoflife = @"
+            var GameOfLife = @"
        +>>++++[<++++>-]<[<++++++>-]+[<[>>>>+<<<<-]>>>>[<<<<+>>>>>>+<<-]<+
    +++[>++++++++<-]>.[-]<+++[>+++<-]>+[>>.+<<-]>>[-]<<<++[<+++++>-]<.<<[>>>>+
  <<<<-]>>>>[<<<<+>>>>>>+<<-]<<[>>>>.+<<<++++++++++[<[>>+<<-]>>[<<+>>>>>++++++++
@@ -56,138 +71,207 @@ namespace TestChallengeTwix
 
 ";
 
-            var hellowworld1 = @"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+            var HelloWorld1 = @"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
 
-            var hellowworld2 = @">++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->
+            var HelloWorld2 = @">++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->
 +++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+.";
 
-            var mandelbrot = File.ReadAllText("mandelbrot.b");
+            var Mandelbrot = File.ReadAllText("mandelbrot.b");
 
-            ExecuteBrainfuck(Console.OpenStandardInput(), Console.OpenStandardOutput(), new MemoryStream(Encoding.ASCII.GetBytes(mandelbrot)));
+            var start = DateTime.UtcNow;
+            InterpretBrainfuck(Mandelbrot,
+                Console.In
+            //new StreamReader(File.OpenRead(@"D:\c\BrainFuck-Interpreter-Challenge\gameoflife-input.txt"))
+            );
+            Console.WriteLine();
+            Console.WriteLine($"Took {(DateTime.UtcNow - start).TotalSeconds:0} sec.");
+            Console.ReadLine();
         }
 
-        public unsafe static void ExecuteBrainfuck(Stream stdinput, Stream stdoutput, Stream brainf)
+        enum Instr
         {
-            const int NUM_CELLS = 16384;
-            const int INPUT_BUFFER_SIZE = 32768;
-            const int OUTPUT_BUFFER_SIZE = 256;
+            Noop,
+            PtrLeft,
+            PtrRight,
+            Add,
+            Sub,
+            AddMultL,
+            AddMultR,
+            SubMultL,
+            SubMultR,
+            SetZero,
+            Input,
+            Output,
+            Jz,
+            Jnz
+        }
 
-            const byte POINTER_RIGHT = 0x3E;
-            const byte POINTER_LEFT = 0x3C;
-            const byte INCREMENT = 0x2B;
-            const byte DECREMENT = 0x2D;
-            const byte WRITE_OUT = 0x2E;
-            const byte READ_IN = 0x2C;
-            const byte LOOP_START = 0x5B;
-            const byte LOOP_END = 0x5D;
+        public unsafe static void InterpretBrainfuck(string bf, TextReader input)
+        {
+            var output = new MemoryStream();
 
-            // outputs
-            byte[] output = new byte[OUTPUT_BUFFER_SIZE];
-            int outputLength = 0;
-            void flushOutput()
+            var blocks = new Stack<List<int>>();
+            var code = new List<int>();
+            var offset = 0;
+
+            var i = 0;
+            for (; i < bf.Length; i++)
             {
-                stdoutput.Write(output, 0, outputLength);
-                outputLength = 0;
-            }
-
-            // inputs
-            byte[] buffer = new byte[INPUT_BUFFER_SIZE];
-            int bufferLength = 0;
-            while (true)
-            {
-                int read = brainf.Read(buffer, bufferLength, buffer.Length - bufferLength);
-                bufferLength += read;
-                if (read <= 0) break;
-            }
-
-            // loop cache
-            int[] lookup = new int[bufferLength];
-            Stack<int> unmatched = new Stack<int>();
-            void addMatch(int start, int end)
-            {
-                lookup[start] = end;
-                lookup[end] = start;
-            }
-
-            byte* cell = stackalloc byte[NUM_CELLS];
-            cell += (NUM_CELLS / 2); // set current cell to somewhere in middle to support negative cell positions
-
-            for (int cursor = 0; cursor < bufferLength; cursor++)
-            {
-                switch (buffer[cursor])
+                switch (bf[i])
                 {
-                    case POINTER_RIGHT: // >   move pointer right
-                        ++cell;
-                        continue;
-                    case POINTER_LEFT: // <   move pointer left
-                        --cell;
-                        continue;
-                    case INCREMENT: // +   increment current cell
-                        ++*cell;
-                        continue;
-                    case DECREMENT: // -   decrement current cell
-                        --*cell;
-                        continue;
-                    case WRITE_OUT: // .   write cell to output
-                        output[outputLength] = *cell;
-                        outputLength++;
-                        if (outputLength >= OUTPUT_BUFFER_SIZE)
-                            flushOutput();
-                        continue;
-                    case READ_IN: // ,   read input to cell
-                        *cell = (byte)stdinput.ReadByte();
-                        continue;
-                    case LOOP_START: // [   Jump past the matching ] if the cell at the pointer is 0
-                        var pairf = lookup[cursor];
-                        if (pairf != 0)
+                    case '.': code.Add((int) Instr.Output); break;
+                    case ',': code.Add((int) Instr.Input); break;
+
+                    case '<':
+                    case '>':
+                    case '+':
+                    case '-':
+                        var j = i + 1;
+                        while (j < bf.Length && bf[j] == bf[i])
+                            j++;
+                        var len = j - i;
+                        switch (bf[i])
                         {
-                            if (*cell == 0) cursor = pairf;
+                            case '<': code.Add((int) Instr.PtrLeft | (len << 4)); break;
+                            case '>': code.Add((int) Instr.PtrRight | (len << 4)); break;
+                            case '+': code.Add((int) Instr.Add | (len << 4)); break;
+                            case '-': code.Add((int) Instr.Sub | (len << 4)); break;
                         }
-                        else
+                        i = j - 1;
+                        break;
+
+                    case '[':
+                        offset += code.Count + (blocks.Count == 0 ? 0 : 1);
+                        blocks.Push(code);
+                        code = new List<int>();
+                        break;
+
+                    case ']':
+                        // Analyze if `code` is reducible to CopyMult/SetZero
+                        var brackets = true;
+                        var ptrOffset = 0;
+                        var intOffset = 0;
+                        var instrs = new List<int>();
+                        var invalid = false;
+                        for (var k = 0; k < code.Count; k++)
                         {
-                            if (*cell == 0) // we need to lookahead for the corresponding closing bracket to jmp to
+                            var it = code[k];
+                            switch ((Instr) (it & 0xf))
                             {
-                                for (int y = cursor; ; y++)
-                                {
-                                    if (buffer[y] == LOOP_START)
-                                    {
-                                        unmatched.Push(y);
-                                    }
-                                    else if (buffer[y] == LOOP_END)
-                                    {
-                                        var x = unmatched.Pop();
-                                        addMatch(x, y);
-                                        if (x == cursor)
-                                        {
-                                            cursor = y;
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
-                            else // we are not jmping here and can initialize this pair lazily 
-                            {
-                                unmatched.Push(cursor);
+                                case Instr.PtrLeft: ptrOffset -= it >> 4; break;
+                                case Instr.PtrRight: ptrOffset += it >> 4; break;
+                                case Instr.Add: if (ptrOffset == 0) intOffset += it >> 4; else instrs.Add((int) (ptrOffset < 0 ? Instr.AddMultL : Instr.AddMultR) | ((it >> 4) << 4) | (Math.Abs(ptrOffset) << 18)); break;
+                                case Instr.Sub: if (ptrOffset == 0) intOffset -= it >> 4; else instrs.Add((int) (ptrOffset < 0 ? Instr.SubMultL : Instr.SubMultR) | ((it >> 4) << 4) | (Math.Abs(ptrOffset) << 18)); break;
+
+                                case Instr.AddMultL:
+                                case Instr.AddMultR:
+                                case Instr.SubMultL:
+                                case Instr.SubMultR:
+                                case Instr.SetZero:
+                                case Instr.Input:
+                                case Instr.Output:
+                                case Instr.Jz:
+                                case Instr.Jnz:
+                                    //invalid = true;
+                                    //break;
+                                    goto invalid;
                             }
                         }
-                        continue;
-                    case LOOP_END: // ]   Jump back to the matching [ if the cell at the pointer is nonzero
-                        var pairb = lookup[cursor];
-                        if (pairb != 0)
+                        if (!invalid && ptrOffset == 0 && intOffset == -1)
                         {
-                            if (*cell != 0) cursor = pairb;
+                            instrs.Add((int) Instr.SetZero);
+                            code = instrs;
+                            brackets = false;
                         }
-                        else
-                        {
-                            var x = unmatched.Pop();
-                            addMatch(x, cursor);
-                            if (*cell != 0) cursor = x;
-                        }
-                        continue;
+                        //else if (ptrOffset == 0)
+                        //{
+                        //    Console.WriteLine(stringifyCode(code));
+                        //}
+                        invalid:;
+
+                        // Pop
+                        var outerCode = blocks.Pop();
+                        var c = outerCode.Count + (blocks.Count == 0 ? 0 : 1);
+                        if (brackets)
+                            outerCode.Add((int) Instr.Jz | ((offset + code.Count + 1) << 4));
+                        outerCode.AddRange(code);
+                        if (brackets)
+                            outerCode.Add((int) Instr.Jnz | (offset << 4));
+                        code = outerCode;
+                        offset -= c;
+                        break;
                 }
             }
 
-            flushOutput();
+            if (blocks.Count != 0)
+                throw new InvalidOperationException("Square brackets are not balanced.");
+
+            //Clipboard.SetText(stringifyCode(code));
+
+            byte* ptr = stackalloc byte[1024 * 16];
+
+            i = 0;
+            unchecked
+            {
+                for (; i < code.Count; i++)
+                {
+                    var instr = code[i];
+                    switch ((Instr) (instr & 0xf))
+                    {
+                        case Instr.PtrLeft: ptr -= instr >> 4; break;
+                        case Instr.PtrRight: ptr += instr >> 4; break;
+                        case Instr.Add: *ptr = (byte) (*ptr + (instr >> 4)); break;
+                        case Instr.Sub: *ptr = (byte) (*ptr - (instr >> 4)); break;
+                        case Instr.AddMultL: *(ptr - (instr >> 18)) += (byte) (((instr >> 4) & 0x3fff) * *ptr); break;
+                        case Instr.AddMultR: *(ptr + (instr >> 18)) += (byte) (((instr >> 4) & 0x3fff) * *ptr); break;
+                        case Instr.SubMultL: *(ptr - (instr >> 18)) -= (byte) (((instr >> 4) & 0x3fff) * *ptr); break;
+                        case Instr.SubMultR: *(ptr + (instr >> 18)) -= (byte) (((instr >> 4) & 0x3fff) * *ptr); break;
+                        case Instr.SetZero: *ptr = 0; break;
+                        case Instr.Input:
+                            int ch;
+                            while ((ch = input.Read()) == 13) ;     // intentionally ignore '\r'
+                            *ptr = (byte) ch;
+                            break;
+
+                        case Instr.Output:
+                            /*
+                            // Output linewise only
+                            if (*ptr == '\n')
+                            {
+                                Console.WriteLine(output.ToArray().FromUtf8());
+                                output = new MemoryStream();
+                            }
+                            else
+                                output.WriteByte(*ptr);
+                            /*/
+                            // Output every character
+                            Console.Write((char) *ptr);
+                            /**/
+                            break;
+                        case Instr.Jz: if (*ptr == 0) i = instr >> 4; break;
+                        case Instr.Jnz: if (*ptr != 0) i = instr >> 4; break;
+                    }
+                }
+            }
         }
+
+        private static string stringifyCode(List<int> code) => string.Join(" ", code.Select((i, ix) => ((Instr) (i & 0xf)) switch
+        {
+            Instr.Noop => $"/",
+            Instr.PtrLeft => $"<{i >> 4}",
+            Instr.PtrRight => $">{i >> 4}",
+            Instr.Add => $"+{i >> 4}",
+            Instr.Sub => $"-{i >> 4}",
+            Instr.AddMultL => $"+{(i >> 4) & 0x3fff}×<{i >> 18}",
+            Instr.AddMultR => $"+{(i >> 4) & 0x3fff}×>{i >> 18}",
+            Instr.SubMultL => $"-{(i >> 4) & 0x3fff}×<{i >> 18}",
+            Instr.SubMultR => $"-{(i >> 4) & 0x3fff}×>{i >> 18}",
+            Instr.SetZero => "0",
+            Instr.Input => ",",
+            Instr.Output => ".",
+            Instr.Jz => $"{ix}[{i >> 4}",
+            Instr.Jnz => $"{ix}]{i >> 4}",
+            _ => "?",
+        }));
     }
 }
---
+++ StringExtensions.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Text;
+
+namespace BrainFJit
+{
+    /// <summary>Provides extension methods on the <see cref="string"/> type.</summary>
+    static class StringExtensions
+    {
+        /// <summary>
+        ///     Converts the specified string to UTF-8.</summary>
+        /// <param name="input">
+        ///     String to convert to UTF-8.</param>
+        /// <returns>
+        ///     The specified string, converted to a byte-array containing the UTF-8 encoding of the string.</returns>
+        public static byte[] ToUtf8(this string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            return Encoding.UTF8.GetBytes(input);
+        }
+
+        /// <summary>
+        ///     Converts the specified string to UTF-16.</summary>
+        /// <param name="input">
+        ///     String to convert to UTF-16.</param>
+        /// <returns>
+        ///     The specified string, converted to a byte-array containing the UTF-16 encoding of the string.</returns>
+        public static byte[] ToUtf16(this string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            return Encoding.Unicode.GetBytes(input);
+        }
+
+        /// <summary>
+        ///     Converts the specified string to UTF-16 (Big Endian).</summary>
+        /// <param name="input">
+        ///     String to convert to UTF-16 (Big Endian).</param>
+        /// <returns>
+        ///     The specified string, converted to a byte-array containing the UTF-16 (Big Endian) encoding of the string.</returns>
+        public static byte[] ToUtf16BE(this string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            return Encoding.BigEndianUnicode.GetBytes(input);
+        }
+
+        /// <summary>
+        ///     Converts the specified raw UTF-8 data to a string.</summary>
+        /// <param name="input">
+        ///     Data to interpret as UTF-8 text.</param>
+        /// <param name="removeBom">
+        ///     <c>true</c> to remove the first character if it is a UTF-8 BOM.</param>
+        /// <returns>
+        ///     A string containing the characters represented by the UTF-8-encoded input.</returns>
+        public static string FromUtf8(this byte[] input, bool removeBom = false)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            var result = Encoding.UTF8.GetString(input);
+            if (removeBom && result[0] == '\ufeff')
+                return result.Substring(1);
+            return result;
+        }
+
+        /// <summary>
+        ///     Converts the specified raw UTF-16 (little-endian) data to a string.</summary>
+        /// <param name="input">
+        ///     Data to interpret as UTF-16 text.</param>
+        /// <returns>
+        ///     A string containing the characters represented by the UTF-16-encoded input.</returns>
+        public static string FromUtf16(this byte[] input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            return Encoding.Unicode.GetString(input);
+        }
+
+        /// <summary>
+        ///     Converts the specified raw UTF-16 (big-endian) data to a string.</summary>
+        /// <param name="input">
+        ///     Data to interpret as UTF-16BE text.</param>
+        /// <returns>
+        ///     A string containing the characters represented by the UTF-16BE-encoded input.</returns>
+        public static string FromUtf16BE(this byte[] input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            return Encoding.BigEndianUnicode.GetString(input);
+        }
+
+        /// <summary>
+        ///     Determines the length of the UTF-8 encoding of the specified string.</summary>
+        /// <param name="input">
+        ///     String to determined UTF-8 length of.</param>
+        /// <returns>
+        ///     The length of the string in bytes when encoded as UTF-8.</returns>
+        public static int Utf8Length(this string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            return Encoding.UTF8.GetByteCount(input);
+        }
+
+        /// <summary>
+        ///     Determines the length of the UTF-16 encoding of the specified string.</summary>
+        /// <param name="input">
+        ///     String to determined UTF-16 length of.</param>
+        /// <returns>
+        ///     The length of the string in bytes when encoded as UTF-16.</returns>
+        public static int Utf16Length(this string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            return Encoding.Unicode.GetByteCount(input);
+        }
+    }
+}
