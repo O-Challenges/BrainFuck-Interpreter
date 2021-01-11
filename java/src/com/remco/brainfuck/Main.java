cadc7c66013c62aef52670e9c60d0f432b557e7d
GameLawl <gaminglawl@gmail.com>
2021-01-11 01:22:29 +0100

    Add Main class with build instructions and manifest file

---
+++ java/src/com/remco/brainfuck/Main.java
@@ -0,0 +1,12 @@
+package com.remco.brainfuck;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger("BrainFuck Main");
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello.");
+    }
+}
