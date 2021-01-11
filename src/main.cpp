a89a6ca5b1c13211198cf3368e1b9b368f1fab60
yasserhcn <hacini_yasser@yahoo.fr>
2021-01-11 20:26:20 +0100

    fetching and decoding

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,60 @@
 // g++ src/main.cpp -o build/interpreter.exe
 
 #include <iostream>
+#include <string>
+
+#define BUFFER_SIZE 30000
+#define MAX_LOOP_STACK 15000
 
 int main()
 {
-    std::cout<<"hello world\n";
+    char buffer[BUFFER_SIZE];
+    std::string instructions = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+    char *returnAdress[MAX_LOOP_STACK];
+    char *instructionPtr = &instructions[0];
+    bool ended = false;
+    int loopStackTrack = 0;
+    int bufferPos = 0;
+
+    while (!ended)
+    {
+        // TODO: fetch
+        char currentInstruction = *instructionPtr;
+
+        // TODO: decode/execute
+        switch (currentInstruction)
+        {
+        case '+':
+            buffer[bufferPos]++;
+            break;
+        case '-':
+            buffer[bufferPos]--;
+            break;
+        case '>':
+            bufferPos++;
+            break;
+        case '<':
+            bufferPos--;
+            break;
+        case '.':
+            std::cout<<buffer[bufferPos];
+            break;
+        case '[':
+            returnAdress[loopStackTrack] = instructionPtr;
+            break;
+        case ']':
+            if(buffer[bufferPos])
+            {
+                
+            }
+            break;
+        
+        default:
+            std::cerr<<"syntax error\n";
+            break;
+        }
+    }
+    
+
     return 0;
 }
