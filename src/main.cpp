86f062f4dd19095b637bcda38f49d75bf7a5f6af
yasserhcn <hacini_yasser@yahoo.fr>
2021-01-12 14:29:51 +0100

    replaced some stuff

--- src/main.cpp
+++ src/main.cpp
@@ -9,53 +9,25 @@
 
 int main()
 {
-    char buffer[BUFFER_SIZE];
-    std::string instructions = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-    char *returnAdress[MAX_LOOP_STACK];
-    char *instructionPtr = &instructions[0];
-    bool ended = false;
-    int loopStackTrack = 0;
-    int bufferPos = 0;
-
-    while (!ended)
-    {
-        // TODO: fetch
-        char currentInstruction = *instructionPtr;
-
-        // TODO: decode/execute
-        switch (currentInstruction)
-        {
-        case '+':
-            buffer[bufferPos]++;
-            break;
-        case '-':
-            buffer[bufferPos]--;
-            break;
-        case '>':
-            bufferPos++;
-            break;
-        case '<':
-            bufferPos--;
-            break;
-        case '.':
-            std::cout<<buffer[bufferPos];
-            break;
-        case '[':
-            returnAdress[loopStackTrack] = instructionPtr;
-            break;
-        case ']':
-            if(buffer[bufferPos])
-            {
-                
-            }
-            break;
-        
-        default:
-            std::cerr<<"syntax error\n";
-            break;
-        }
-    }
-    
-
     return 0;
 }
+
+class bfShell
+{
+public:
+    bfShell()
+    {
+        char* buffer = new char[BUFFER_SIZE];
+        bufferStrip = buffer;
+    }
+
+    ~bfShell()
+    {
+        delete bufferStrip;
+    }
+
+private:
+    char* bufferStrip;
+    char instructions[100];
+    char* instructionIndex = &instructions[0];
+};
